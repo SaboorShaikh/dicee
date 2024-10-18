5c6acfb776b3a1be27acf9ed194badd4205b1657
SaboorShaikh <130291554+SaboorShaikh@users.noreply.github.com>
2024-10-18 11:38:09 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              "Dicee",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+                onPressed: () {},
+                child: Image.asset("assets/images/dice1.png")),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("assets/images/dice2.png"),
+          )),
+        ],
+      ),
+    );
+  }
+}
